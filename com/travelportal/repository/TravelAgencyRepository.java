fb1b9c1d51b41ff9c3e342e24f0977327c723934
Shahzad Khan <shahzad.sdr60@gmail.com>
2025-08-22 16:00:20 +0530

    first commit

--- com/travelportal/repository/TravelAgencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travelportal.repository;
-
-import com.travelportal.model.TravelAgency;
-//import org.springframework.data.jpa.repository.JpaRepository; ((baad me sahi krna hai Insha Allah))
-
-//incomplete interface for extand with JpaRepository ((baad me sahi krna hai Insha Allah))
-public interface TravelAgencyRepository <TravelAgency, Long> {
-    TravelAgency findByUsername(String username);
-}
